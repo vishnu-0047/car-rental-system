21f18a9f4ea3fc9b7933abbaa0182ea4ff5f686b
vishnu-0047 <vishnupriy4n@gmail.com>
2026-08-08 23:34:20 +0530

    Create DBConnection.java

---
+++ src/util/DBConnection.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:oracle:thin:@localhost:1521/XE";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                URL,
+                USERNAME,
+                PASSWORD
+        );
+        System.out.println("Connected to Oracle database!");
+        return connection;
+    }
+}
